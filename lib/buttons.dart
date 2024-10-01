2e2428d43f08525923649b351042e609c246d2cc
jgalicia-dev <javiergaliciaglz@gmail.com>
2024-09-30 18:07:19 -0600

    Manejo de espacio en los botones.

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -6,28 +6,37 @@ class Buttons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              const SizedBox(height: 100,),
+              //const SizedBox(height: 100,),
               const Text('Aqui se van a mostrar los valores'),              
-              Row(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('7')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('8')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('9')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('+')),
               ],),
-              Row(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('4')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('5')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('6')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('-')),
               ],),
-              Row(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('1')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('2')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('3')),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('*')),
               ],),
-              Row(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                 const SizedBox(width: 60,),
                 ElevatedButton(onPressed: (){ /**/ }, child: const Text('0')),
                 const SizedBox(width: 50,),
